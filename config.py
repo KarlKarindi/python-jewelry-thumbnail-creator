40975911dbd31717d4f10a17026bdad4f6d0aa83
KarlKarindi <karl_erik97@hotmail.com>
2020-12-13 14:30:00 +0200

    dynamic white bg

---
+++ config.py
@@ -0,0 +1,12 @@
+
+OFFSET = 500
+
+THRESHOLD = 100
+USE_DIST = False
+DIST = 5
+
+UNCROPPABLE_PICTURES_LOC = "C:/Users/Karl/Desktop/resizing/not_to_crop.txt"
+PICTURES_DIR_IN = "C:/Users/Karl/Desktop//"
+PICTURES_DIR_OUT = "C:/Users/Karl/Desktop/Pildid_2_600x600/"
+TEMP_DIR = "C:/Users/Karl/Desktop/resizing/"
+TEMP_DIR_OUT = "C:/Users/Karl/Desktop/resizing/out/"
--- resizer.py
+++ resizer.py
@@ -1,4 +1,5 @@
 from PIL import Image
+from config import PICTURES_DIR_OUT, PICTURES_DIR_IN, UNCROPPABLE_PICTURES_LOC, TEMP_DIR_OUT, TEMP_DIR, THRESHOLD, DIST, USE_DIST, OFFSET
 from os import listdir
 from os.path import isfile, join
 import numpy as np
@@ -13,31 +14,30 @@ class CropInfo(object):
     def __str__(self):
         return "X_MIN: {}, X_MAX: {}, Y_MIN: {}, Y_MAX: {}".format(self.X_MIN, self.X_MAX, self.Y_MIN, self.Y_MAX)
 
+
 def resize(cropped_img, pic_name):
     new_img = cropped_img.resize((600, 600))
-    new_img.save(PICTURES_DIR_OUT + pic_name.strip(".jpg") + ".png", optimize=True)
+    new_img.save(PICTURES_DIR_OUT + pic_name, optimize=True)
+
 
 def crop_image(img):
     pixels = img.load()
     width, height = img.width - 1, img.height - 1
-    threshold = 250
-    dist = 5
-    
+
     crop_info = CropInfo()
     for x in range(0, width):
         for y in range(0, height):
             pixel = pixels[x, y]
-            if not pixel_is_white(pixel, threshold, dist):
+            if not pixel_is_white(pixel):
                 crop_info = extract_info(crop_info, x, y)
 
     width = crop_info.X_MAX - crop_info.X_MIN
     height = crop_info.Y_MAX - crop_info.Y_MIN
-    
-    #print(crop_info)
-    
+
     crop_info = fix_incorrect_aspect_ratio(crop_info, width, height)
     cropped_img = img.crop((crop_info.X_MIN, crop_info.Y_MIN, crop_info.X_MAX, crop_info.Y_MAX))
     return cropped_img
+
 
 def extract_info(crop_info, x, y):
     if x < crop_info.X_MIN:
@@ -50,6 +50,7 @@ def extract_info(crop_info, x, y):
         crop_info.Y_MAX = y
     return crop_info
 
+
 def fix_incorrect_aspect_ratio(info, width, height):
     if width > height:
         offset = (width - height) / 2
@@ -59,66 +60,65 @@ def fix_incorrect_aspect_ratio(info, width, height):
         offset = (height - width) / 2
         info.X_MIN -= offset
         info.X_MAX += offset
-        
-    
+
     crop_width = info.X_MAX - info.X_MIN
-    offset = 150
     x_offset_extra = 0
     if crop_width < 600:
         x_offset_extra = (600 - crop_width) / 2
-    info.X_MIN -= (offset + x_offset_extra)
-    info.X_MAX += (offset + x_offset_extra)
-    
+    info.X_MIN -= (OFFSET + x_offset_extra)
+    info.X_MAX += (OFFSET + x_offset_extra)
+
     crop_height = info.Y_MAX - info.Y_MIN
     y_offset_extra = 0
     if crop_height < 600:
         y_offset_extra = (600 - crop_height) / 2
-    info.Y_MIN -= (offset + y_offset_extra)
-    info.Y_MAX += (offset + y_offset_extra)
+    info.Y_MIN -= (OFFSET + y_offset_extra)
+    info.Y_MAX += (OFFSET + y_offset_extra)
 
     return info
 
-def pixel_is_white(pixel, threshold, dist):
-    r,g,b = pixel[0], pixel[1], pixel[2]
-    first_test_pass = ((r >= threshold)
-            & (g >= threshold)
-            & (b >= threshold))
-    return first_test_pass
-    
-    if first_test_pass:
-        return ((np.abs(r-g)<dist)
-            & (np.abs(r-b)<dist)
-            & (np.abs(g-b)<dist))
-    
-    return False
+
+def pixel_is_white(pixel):
+    r, g, b = pixel[0], pixel[1], pixel[2]
+    first_test_pass = ((r >= THRESHOLD)
+                       and (g >= THRESHOLD)
+                       and (b >= THRESHOLD))
+
+    if not USE_DIST:
+        return first_test_pass
+
+    return ((np.abs(r - g) < DIST)
+            and (np.abs(r - b) < DIST)
+            and (np.abs(g - b) < DIST))
+
 
 def paste_to_background(img, background, bg_w, bg_h, img_name):
     img_w, img_h = img.size
     offset = ((bg_w - img_w) // 2, (bg_h - img_h) // 2)
     background.paste(img, offset)
     background.save(TEMP_DIR_OUT + "OUT_" + img_name + ".png")
-    
 
-PICTURES_DIR_IN = "C:/Users/Karl/Desktop/Ehete pildid/"
-#PICTURES_DIR_IN = "C:/Users/Karl/Desktop/test/"
-PICTURES_DIR_OUT = "C:/Users/Karl/Desktop/Pildid_1_600x600/"
-TEMP_DIR = "C:/Users/Karl/Desktop/resizing/"
-TEMP_DIR_OUT = "C:/Users/Karl/Desktop/resizing/out/"
+
 pic_files = [f for f in listdir(PICTURES_DIR_IN) if isfile(join(PICTURES_DIR_IN, f))]
+NOT_TO_CROP = [f for f in open(UNCROPPABLE_PICTURES_LOC).readlines()]
 
-background = Image.new('RGB', (2500, 2500), (255, 255, 255))
-bg_w, bg_h = background.size
 print("Resizing", len(pic_files), "pictures\n")
-
-for pic_name in pic_files:
-    if pic_name.endswith(".jpg"):
-        #print("Resizing picture", pic_name)
-        pic_name_without_jpg = pic_name.strip(".jpg")
+for i, pic_name in enumerate(pic_files, 1):
+    if pic_name.lower().endswith(".jpg"):
+        pic_name_without_jpg = pic_name.split(".")[0]
         original_img = Image.open(PICTURES_DIR_IN + pic_name)
-        paste_to_background(original_img, background, bg_w, bg_h, pic_name_without_jpg)
-        img_with_background = Image.open(TEMP_DIR_OUT + "OUT_" + pic_name_without_jpg + ".png")
-        cropped_img = crop_image(img_with_background)
-        resize(cropped_img, pic_name)
-        #print()
+        if pic_name in NOT_TO_CROP:
+            resize(original_img, pic_name)
+        else:
+            white_bg_size = original_img.width
+            background = Image.new('RGB', (2 * OFFSET + white_bg_size, 2 * OFFSET + white_bg_size), (255, 255, 255))
+            bg_w, bg_h = background.size
+            paste_to_background(original_img, background, bg_w, bg_h, pic_name_without_jpg)
+            img_with_background = Image.open(TEMP_DIR_OUT + "OUT_" + pic_name_without_jpg + ".png")
+            cropped_img = crop_image(img_with_background)
+            resize(cropped_img, pic_name_without_jpg + ".png")
 
-print("Resizing completed")
+        print("Resized picture #" + str(i) + ":", pic_name,
+              original_img.size, "- Saved as:", pic_name_without_jpg + ".png")
+
+print("Resizing completed!")
