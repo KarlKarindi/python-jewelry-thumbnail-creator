51523279987c69ae47bef6070b256d0e3f23652d
KarlKarindi <karl_erik97@hotmail.com>
2021-01-26 10:42:38 +0200

    Add comment

--- config.py
+++ config.py
@@ -3,14 +3,14 @@ OFFSET = 300
 WHITE_BG_ADDITIONAL = 3 * OFFSET
 
 # Set threshold to 100 if it's a self-made picture
-THRESHOLD = 250
+THRESHOLD = 100
 USE_DIST = False
 DIST = 5
 
 UNCROPPABLE_PICTURES_LOC = "C:/Users/Karl/Desktop/resizing/not_to_crop.txt"
-PICTURES_DIR_IN = "C:/Users/Karl/Desktop/wetransfer-8d7bb4/08.12.2020/"
-PICTURES_DIR_OUT = "C:/Users/Karl/Desktop/Pildid_2_600x600/"
+PICTURES_DIR_IN = "C:/Users/Karl/Desktop/wetransfer-50aeb8/Charlowe 2/"
+PICTURES_DIR_OUT = "C:/Users/Karl/Desktop/Charlowe 2/"  # Must add folder to save as well if it doesn't exist
 TEMP_DIR = "C:/Users/Karl/Desktop/resizing/"
 TEMP_DIR_OUT = "C:/Users/Karl/Desktop/resizing/out/"
 
-SAVE_FORMAT = ".jpg"
+SAVE_FORMAT = ".png"
