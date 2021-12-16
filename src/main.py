ca6ee837f49bde9909c374eda7dd0ecad78a8807
KarlKarindi <karl_erik97@hotmail.com>
2021-12-17 01:10:39 +0200

    Try otsu thresholding

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import numpy as np
 import os
 import glob
 import time
-import cv2 as cv
+import cv2
 from matplotlib import pyplot as plt
 
 
@@ -44,19 +44,39 @@ def execute(pictures_dir_in, pictures_dir_out):
 
 
 def find_crop_coords(abspath):
-    img = cv.imread(abspath, flags=0)
-    img_blur = cv.blur(img, (5,5))   
+    img = cv2.imread(abspath)
+    original = img.copy()
+    img_blur = cv2.blur(img, (5,5))   
     #edges = cv.Canny(img, 60, 50)
-    grad_x = cv.Sobel(img, cv.CV_64F, 1, 0)
-    grad_y = cv.Sobel(img, cv.CV_64F, 0, 1)
-    grad = np.sqrt(grad_x**2 + grad_y**2)
-    grad_norm = (grad * 255 / grad.max()).astype(np.uint8)
+    mask = np.zeros(img.shape, dtype=np.uint8)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.threshold(gray, 0, 10, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2,2))
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
+
+    cnts = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+    for c in cnts:
+        cv2.drawContours(mask, [c], -1, (255,255,255), -1)
+        break
+
+    close = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=4)
+    close = cv2.cvtColor(close, cv2.COLOR_BGR2GRAY)
+    result = cv2.bitwise_and(original, original, mask=close)
+    result[close==0] = (255,255,255)
+
+    cv2.namedWindow('custom window', cv2.WINDOW_KEEPRATIO)
+    cv2.imshow('custom window', result)
+    cv2.resizeWindow('custom window', 800  , 800)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     
     
     
     #indices = np.where(edges != [0])
 
-    indices = np.nonzero(grad_norm)
+    indices = np.nonzero(thresh)
     
     ci = CropInfo(
         min(indices[1]) - 250,
